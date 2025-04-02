41125dab09552fae18acf45a35b9a936e8b0d99e
vat03 <vatsoni123456@gmail.com>
2025-04-02 13:49:22 +0530

    Updated Entities

--- Loan-Management-System/src/main/java/com/aurionpro/lms/entity/Admin.java
+++ Loan-Management-System/src/main/java/com/aurionpro/lms/entity/Admin.java
@@ -1,27 +1,3 @@
-//package com.aurionpro.lms.entity;
-//
-//import java.util.List;
-//
-//import jakarta.persistence.CascadeType;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.FetchType;
-//import jakarta.persistence.OneToMany;
-//import jakarta.persistence.PrimaryKeyJoinColumn;
-//import jakarta.persistence.Table;
-//import lombok.AllArgsConstructor;
-//import lombok.RequiredArgsConstructor;
-//
-//@AllArgsConstructor
-//@RequiredArgsConstructor
-//@Entity
-//@Table(name = "admin")
-//@PrimaryKeyJoinColumn(name = "id") // Ensuring proper inheritance
-//public class Admin extends UserType {
-//
-//	@OneToMany(mappedBy = "admin", cascade = { CascadeType.MERGE, CascadeType.PERSIST }, fetch = FetchType.LAZY)
-//	private List<LoanOfficer> loanOfficers;
-//}
-
 package com.aurionpro.lms.entity;
 
 import java.util.List;
