be29277764265e1a7ee027ac4099adb1b9e1bd8b
vat03 <vatsoni123456@gmail.com>
2025-04-13 16:54:28 +0530

    Updated FE

--- Loan-Management-System-Frontend/LMS_FE/src/app/app.component.ts
+++ Loan-Management-System-Frontend/LMS_FE/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  standalone: false,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
