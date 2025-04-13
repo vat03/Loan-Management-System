da64aa4217aade4d1bdae71e0f9d69ffc97ab99b
vat03 <vatsoni123456@gmail.com>
2025-04-13 22:55:40 +0530

    Updated FrontEnd Dashboards

--- Loan-Management-System-Frontend/LMS_FE/src/app/app.config.ts
+++ Loan-Management-System-Frontend/LMS_FE/src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { authInterceptor } from './core/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations()
+  ]
 };
--- Loan-Management-System-Frontend/LMS_FE/src/app/app.routes.ts
+++ Loan-Management-System-Frontend/LMS_FE/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '././core/auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'admin',
+        loadChildren: () => import('./admin/admin-routes').then(m => m.ADMIN_ROUTES),
+        canActivate: [AuthGuard],
+        data: { roles: ['ROLE_ADMIN'] }
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+      },
+      { path: '', redirectTo: '/login', pathMatch: 'full' },
+      { path: '**', redirectTo: '/login' }
+];
