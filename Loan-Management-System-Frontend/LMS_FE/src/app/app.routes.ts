ffaad7c5f7471d421d2f773ee4d061cb842052ae
vat03 <vatsoni123456@gmail.com>
2025-04-15 20:12:52 +0530

    Updated routes

--- Loan-Management-System-Frontend/LMS_FE/src/app/app.routes.ts
+++ Loan-Management-System-Frontend/LMS_FE/src/app/app.routes.ts
@@ -47,6 +47,46 @@
 // ];
 
 
+import { Routes } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProfileComponent } from './shared/components/profile/profile.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin-routes').then(m => m.ADMIN_ROUTES),
+    canActivate: [AuthGuard],
+    data: { roles: ['ROLE_ADMIN'] }
+  },
+  {
+    path: 'loan-officer',
+    loadChildren: () => import('./loan-officer/loan-officer-routes').then(m => m.LOAN_OFFICER_ROUTES),
+    canActivate: [AuthGuard],
+    data: { roles: ['ROLE_LOAN_OFFICER'] }
+  },
+  {
+    path: 'customer',
+    loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule),
+    canActivate: [AuthGuard],
+    data: { roles: ['ROLE_CUSTOMER'] }
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+  },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'profile',
+    canActivate: [AuthGuard],
+    data: { roles: ['ROLE_ADMIN', 'ROLE_LOAN_OFFICER', 'ROLE_CUSTOMER'] },
+    component: ProfileComponent
+  },
+  { path: '**', redirectTo: '' }
+];
+
 // import { Routes } from '@angular/router';
 // import { AuthGuard } from './core/auth/auth.guard';
 // import { HomeComponent } from './home/home.component';
@@ -87,44 +127,3 @@
 //   },
 //   { path: '**', redirectTo: '' }
 // ];
-
-import { Routes } from '@angular/router';
-import { AuthGuard } from './core/auth/auth.guard';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProfileComponent } from './shared/components/profile/profile.component';
-import { LoginComponent } from './auth/login/login.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'admin',
-    loadChildren: () => import('./admin/admin-routes').then(m => m.ADMIN_ROUTES),
-    canActivate: [AuthGuard],
-    data: { roles: ['ROLE_ADMIN'] }
-  },
-  {
-    path: 'loan-officer',
-    loadChildren: () => import('./loan-officer/loan-officer-routes').then(m => m.LOAN_OFFICER_ROUTES),
-    canActivate: [AuthGuard],
-    data: { roles: ['ROLE_LOAN_OFFICER'] }
-  },
-  {
-    path: 'customer',
-    loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule),
-    canActivate: [AuthGuard],
-    data: { roles: ['ROLE_CUSTOMER'] }
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
-  },
-  { path: 'register', component: RegisterComponent },
-  {
-    path: 'profile',
-    canActivate: [AuthGuard],
-    data: { roles: ['ROLE_ADMIN', 'ROLE_LOAN_OFFICER', 'ROLE_CUSTOMER'] },
-    component: ProfileComponent
-  },
-  { path: '**', redirectTo: '' }
-];
